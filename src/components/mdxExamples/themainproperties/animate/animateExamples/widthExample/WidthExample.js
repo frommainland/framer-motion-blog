5843cbda87b39e58f103a764e98f0a9a6ece5a5b
shuo <caihehuang@gmail.com>
2024-01-02 23:48:30 +0800

    width example with styles

--- src/components/mdxExamples/themainproperties/animate/animateExamples/widthExample/WidthExample.js
+++ src/components/mdxExamples/themainproperties/animate/animateExamples/widthExample/WidthExample.js
@@ -4,12 +4,18 @@ import styles from './WidthExample.module.scss'
 import { motion } from 'framer-motion'
 
 export default function WidthExample() {
+    const [isHover, setIsHover] = React.useState(false)
     return (
-        <motion.div className={styles.wrapper} >
-            <motion.div className={styles.innerWrap} >
-                <motion.div className={styles.ring} whileTap={{ width: 64, height: 128 }}></motion.div>
-            </motion.div>
-        </motion.div>
+        <div className={styles.container}>
+            <div className={styles.wrapper} >
+                <motion.div className={styles.innerWrap} onHoverStart={() => setIsHover(true)} onHoverEnd={() => setIsHover(false)} whileHover={{ backgroundColor: 'var(--color-accent-green)' }}>
+                    <motion.div className={styles.ring}
+                        animate={{ width: isHover ? '3vw' : '1vw', height: isHover ? '4vw' : '1vw' }}></motion.div>
+                </motion.div>
+            </div>
+            <p>height & width</p>
+        </div>
+
     )
 }
 

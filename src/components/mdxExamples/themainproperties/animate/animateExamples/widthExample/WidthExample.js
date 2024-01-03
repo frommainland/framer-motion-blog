1a512a03da2fda77722a453beb4b5ad135b79b38
shuo <caihehuang@gmail.com>
2024-01-03 23:20:29 +0800

    update with AnimateExample wrapper

--- src/components/mdxExamples/themainproperties/animate/animateExamples/widthExample/WidthExample.js
+++ src/components/mdxExamples/themainproperties/animate/animateExamples/widthExample/WidthExample.js
@@ -2,20 +2,20 @@
 import React from 'react'
 import styles from './WidthExample.module.scss'
 import { motion } from 'framer-motion'
+import ExampleWrapper from '../exampleWrapper/ExampleWrapper'
 
 export default function WidthExample() {
     const [isHover, setIsHover] = React.useState(false)
     return (
-        <div className={styles.container}>
+        <ExampleWrapper caption='height & width'>
             <div className={styles.wrapper} >
                 <motion.div className={styles.innerWrap} onHoverStart={() => setIsHover(true)} onHoverEnd={() => setIsHover(false)} whileHover={{ backgroundColor: 'var(--color-accent-green)' }}>
                     <motion.div className={styles.ring}
-                        animate={{ width: isHover ? '3vw' : '1vw', height: isHover ? '4vw' : '1vw' }}></motion.div>
+                        animate={{ width: isHover ? '3vw' : '1vw', height: isHover ? '4vw' : '1vw' }}
+                    ></motion.div>
                 </motion.div>
             </div>
-            <p>height & width</p>
-        </div>
-
+        </ExampleWrapper>
     )
 }
 

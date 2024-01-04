ed53dd4385d05aedc2e05830b5456d1f348bef63
shuo <caihehuang@gmail.com>
2024-01-04 15:04:34 +0800

    new - animate border example

---
+++ src/components/mdxExamples/themainproperties/animate/animateExamples/border/BorderExample.js
@@ -0,0 +1,22 @@
+'use client'
+import { motion, motionValue, useTransform } from 'framer-motion'
+import ExampleWrapper from '../exampleWrapper/ExampleWrapper'
+import styles from './BorderExample.module.scss'
+
+import React from 'react'
+
+export default function BorderExample() {
+    const [isHover, setIsHover] = React.useState(false)
+
+    return (
+        <ExampleWrapper caption='border'>
+            <div className={styles.wrapper} onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+                <motion.div className={styles.ring}
+                    animate={{
+                        borderWidth: isHover ? '10px' : '1px',
+                        filter: isHover ? 'drop-shadow(0px 0px 3.6px #fff) drop-shadow(0px 0px 7.2px #fff) drop-shadow(0px 0px 25.2px #fff) drop-shadow(0px 0px 50.4px #fff) drop-shadow(0px 0px 86.4px #fff) drop-shadow(0px 0px 151.2px #fff)' : 'drop-shadow(0px 0px 0px #2C2D29)'
+                    }}></motion.div>
+            </div>
+        </ExampleWrapper >
+    )
+}

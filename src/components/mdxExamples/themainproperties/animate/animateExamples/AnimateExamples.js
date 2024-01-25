5bfcfe0945b76a718ee42edc7353d2468912d391
shuo <caihehuang@gmail.com>
2024-01-25 22:28:02 +0800

    add more animation examples

--- src/components/mdxExamples/themainproperties/animate/animateExamples/AnimateExamples.js
+++ src/components/mdxExamples/themainproperties/animate/animateExamples/AnimateExamples.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import styles from './AnimateExamples.module.scss'
 import WidthExample from './widthExample/WidthExample'
+import YExample from './yExample/YExample'
+import RotateExample from './rotate/RotateExample'
+import BackgroundColor from './backgroundcolor/BackgroundColor'
+import BorderExample from './border/BorderExample'
+import BoxShadow from './boxshadow/BoxShadow'
+import FontVariation from './fontVariation/FontVariation'
+import Filter from './filter/Filter'
 
 export default function AnimateExamples() {
     return (
-        <div>
+        <div className={styles.gridWrap}>
             <WidthExample />
+            <YExample />
+            <RotateExample />
+            <BackgroundColor />
+            <BorderExample />
+            <BoxShadow />
+            <FontVariation />
+            <Filter />
         </div>
     )
 }

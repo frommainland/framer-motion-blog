da7f60c51745836a928067f793033636fcd42e9c
shuo <caihehuang@gmail.com>
2024-01-27 23:00:06 +0800

    file mis locate fix

---
+++ src/components/mdxExamples/themainproperties/transition/repeatMix/RepeatMix.jsx
@@ -0,0 +1,25 @@
+'use client'
+import React from 'react'
+import styles from './RepeatMix.module.scss'
+import Petal from './Petal'
+import Gauge from './Gauge'
+
+export default function RepeatMix() {
+	const animOptions = {
+		duration: 1.2,
+		repeatDelay: 1,
+	}
+
+	return (
+		<div className={styles.wrap}>
+			<div className={styles.petalWrap}>
+				<Petal repeatType="reverse" animOptions={animOptions} />
+				<Petal repeatType="mirror" animOptions={animOptions} />
+			</div>
+			<div className={styles.gaugeWrap}>
+				<Gauge repeatType="reverse" animOptions={animOptions} />
+				<Gauge repeatType="mirror" animOptions={animOptions} />
+			</div>
+		</div>
+	)
+}
---
+++ src/components/mdxExamples/themainproperties/transition/repeatMix/index.js
@@ -0,0 +1,5 @@
+'use client'
+import dynamic from 'next/dynamic';
+
+const RepeatMix = dynamic(() => import('./RepeatMix'))
+export default RepeatMix

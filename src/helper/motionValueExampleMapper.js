714096638571d855ba18d5cb77b4126378abe335
shuo <caihehuang@gmail.com>
2024-01-30 11:19:14 +0800

    Update motionValueExampleMapper.js

--- src/helper/motionValueExampleMapper.js
+++ src/helper/motionValueExampleMapper.js
@@ -1,7 +1,10 @@
 import InjectMotionValue from "@/components/mdxExamples/motionValue/motionValueBasic/InjectMotionValue/";
-
+import SetMotionValue from "@/components/mdxExamples/motionValue/motionValueBasic/setMotionValue";
+import ShowMotionValue from "@/components/mdxExamples/motionValue/motionValueBasic/ShowMotionValue/";
 const motionValueExampleMapper = {
     InjectMotionValue,
+    SetMotionValue,
+    ShowMotionValue
 }
 
 export default motionValueExampleMapper

7103e2d8f5f28c9b5370c8d904ea1b33ad5904ca
shuo <caihehuang@gmail.com>
2024-03-01 22:31:03 +0800

    new - mdx mapper component for variants

---
+++ src/helper/animationsExampleMapper.js
@@ -0,0 +1,13 @@
+import StaggerChildAndDelayChildren from "@/components/mdxExamples/animations/variants/staggerChild_delayChildren";
+import StaggerDirectionAndWhen from "@/components/mdxExamples/animations/variants/staggerDirection_When";
+import DynamicVariants from "@/components/mdxExamples/animations/variants/DynamicVariants";
+
+import CustomProps from "@/components/mdxExamples/animations/variants/CustomProps";
+const animationExampleMapper = {
+    StaggerChildAndDelayChildren,
+    StaggerDirectionAndWhen,
+    DynamicVariants,
+    CustomProps
+}
+
+export default animationExampleMapper

a407afab17d909a0bcbe74d862832396dfe2ce28
shuo <caihehuang@gmail.com>
2024-03-03 23:32:15 +0800

    keyframe components mdx mapper

--- src/helper/animationsExampleMapper.js
+++ src/helper/animationsExampleMapper.js
@@ -1,13 +1,22 @@
 import StaggerChildAndDelayChildren from "@/components/mdxExamples/animations/variants/staggerChild_delayChildren";
 import StaggerDirectionAndWhen from "@/components/mdxExamples/animations/variants/staggerDirection_When";
 import DynamicVariants from "@/components/mdxExamples/animations/variants/DynamicVariants";
-
+import KeyframeBasic from "@/components/mdxExamples/animations/keyframes/KeyframeBasic";
 import CustomProps from "@/components/mdxExamples/animations/variants/CustomProps";
+import KeyframeMultiProps from "@/components/mdxExamples/animations/keyframes/KeyframeMultiProps";
+import KeyframeAddPause from "@/components/mdxExamples/animations/keyframes/KeyframeAddPause";
+import KeyframeTimesOption from "@/components/mdxExamples/animations/keyframes/KeyframeTimesOption";
+import KeyframeEaseOption from "@/components/mdxExamples/animations/keyframes/KeyframeEaseOption";
 const animationExampleMapper = {
     StaggerChildAndDelayChildren,
     StaggerDirectionAndWhen,
     DynamicVariants,
-    CustomProps
+    CustomProps,
+    KeyframeBasic,
+    KeyframeMultiProps,
+    KeyframeAddPause,
+    KeyframeTimesOption,
+    KeyframeEaseOption,
 }
 
 export default animationExampleMapper

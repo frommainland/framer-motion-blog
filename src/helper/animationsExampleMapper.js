cc73a47a2d19dc4c4340b21e1289824492a3fa32
shuo <caihehuang@gmail.com>
2024-03-05 23:48:30 +0800

    update animationExampleMapper

--- src/helper/animationsExampleMapper.js
+++ src/helper/animationsExampleMapper.js
@@ -7,6 +7,14 @@ import KeyframeMultiProps from "@/components/mdxExamples/animations/keyframes/Ke
 import KeyframeAddPause from "@/components/mdxExamples/animations/keyframes/KeyframeAddPause";
 import KeyframeTimesOption from "@/components/mdxExamples/animations/keyframes/KeyframeTimesOption";
 import KeyframeEaseOption from "@/components/mdxExamples/animations/keyframes/KeyframeEaseOption";
+import AnimatePresenceBasic from "@/components/mdxExamples/animations/exitAnimations/AnimatePresenceBasic";
+import MultiChildren from "@/components/mdxExamples/animations/exitAnimations/MultiChildren";
+import KeyDiff from "@/components/mdxExamples/animations/exitAnimations/KeyDiff";
+import MountEleInCloseUp from "@/components/mdxExamples/animations/exitAnimations/MountEleInCloseUp";
+import ModeWait from "@/components/mdxExamples/animations/exitAnimations/ModeOptions/ModeWait";
+import ModeCompare from "@/components/mdxExamples/animations/exitAnimations/ModeOptions/ModeCompare";
+import OnExitComplete from "@/components/mdxExamples/animations/exitAnimations/OnExitComplete";
+
 const animationExampleMapper = {
     StaggerChildAndDelayChildren,
     StaggerDirectionAndWhen,
@@ -17,6 +25,13 @@ const animationExampleMapper = {
     KeyframeAddPause,
     KeyframeTimesOption,
     KeyframeEaseOption,
+    AnimatePresenceBasic,
+    MultiChildren,
+    KeyDiff,
+    MountEleInCloseUp,
+    ModeWait,
+    ModeCompare,
+    OnExitComplete,
 }
 
 export default animationExampleMapper

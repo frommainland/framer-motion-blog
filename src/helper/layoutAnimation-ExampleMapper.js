e08c8154024c2c335003d090e135b8c65db27337
shuo <caihehuang@gmail.com>
2024-03-28 11:15:31 +0800

    update mdx example mapper

--- src/helper/layoutAnimation-ExampleMapper.js
+++ src/helper/layoutAnimation-ExampleMapper.js
@@ -4,6 +4,10 @@ import ScaleCorrection from "@/components/mdxExamples/layoutAnimations/ScaleCorr
 import CustomLayoutAnimation from "@/components/mdxExamples/layoutAnimations/CustomLayoutAnimation/CustomLayoutAnimation";
 import LayoutValues from "@/components/mdxExamples/layoutAnimations/CustomLayoutAnimation/LayoutValues";
 import LayoutValues02 from "@/components/mdxExamples/layoutAnimations/CustomLayoutAnimation/LayoutValues02";
+import LayoutGroup01 from "@/components/mdxExamples/layoutAnimations/LayoutGroup/LayoutGroup01";
+import LayoutGroup02 from "@/components/mdxExamples/layoutAnimations/LayoutGroup/LayoutGroup02";
+import LayoutGroup03 from "@/components/mdxExamples/layoutAnimations/LayoutGroup/LayoutGroup03";
+
 const layoutAnimationExampleMapper = {
     LayoutAnimationBasic,
     InlinevsCSS,
@@ -11,8 +15,9 @@ const layoutAnimationExampleMapper = {
     CustomLayoutAnimation,
     LayoutValues,
     LayoutValues02,
-    
-
+    LayoutGroup01,
+    LayoutGroup02,
+    LayoutGroup03,
 }
 
 export default layoutAnimationExampleMapper

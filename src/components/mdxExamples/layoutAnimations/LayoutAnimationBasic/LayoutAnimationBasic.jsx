2fea13dfaf541aecdc4b4efa742c22ce8374e3a0
shuo <caihehuang@gmail.com>
2024-03-31 00:13:17 +0800

    modify - css related

--- src/components/mdxExamples/layoutAnimations/LayoutAnimationBasic/LayoutAnimationBasic.jsx
+++ src/components/mdxExamples/layoutAnimations/LayoutAnimationBasic/LayoutAnimationBasic.jsx
@@ -2,59 +2,80 @@
 import React from 'react'
 import styles from './LayoutAnimationBasic.module.scss'
 import { motion } from 'framer-motion'
+import SingleCheckBox from '@/components/SingleCheckBox'
 
 export default function LayoutAnimationBasic() {
 	const [clicked, setClicked] = React.useState(false)
 
+	const [layout, setlayout] = React.useState(true)
+
 	return (
 		<div className={styles.wrap}>
-			<div className={styles.exampleWrap}>
-				<motion.div
-					className={styles.item}
-					style={{
-						justifyContent: clicked ? 'flex-start' : 'flex-end',
-					}}
-					animate={{
-						backgroundColor: clicked
-							? 'var(--color-surface-200)'
-							: 'var(--color-text-100)',
-					}}
-				>
+			{layout ? (
+				<div className={styles.exampleWrap}>
 					<motion.div
-						className={styles.handle}
+						className={styles.item}
+						style={{
+							justifyContent: clicked ? 'flex-start' : 'flex-end',
+						}}
 						animate={{
 							backgroundColor: clicked
-								? 'var(--color-surface-300)'
-								: 'var(--color-accent-green)',
+								? 'var(--color-surface-200)'
+								: 'var(--color-text-100)',
 						}}
-						onClick={() => setClicked(!clicked)}
-					></motion.div>
-				</motion.div>
-			</div>
-			<div className={styles.exampleWrap}>
-				<motion.div
-					className={styles.item}
-					style={{
-						justifyContent: clicked ? 'flex-start' : 'flex-end',
-					}}
-					animate={{
-						backgroundColor: clicked
-							? 'var(--color-surface-200)'
-							: 'var(--color-text-100)',
-					}}
-				>
+					>
+						<motion.div
+							className={styles.handle}
+							animate={{
+								backgroundColor: clicked
+									? 'var(--color-surface-300)'
+									: 'var(--color-accent-green)',
+							}}
+							onClick={() => setClicked(!clicked)}
+							layout
+						></motion.div>
+					</motion.div>
+				</div>
+			) : (
+				<div className={styles.exampleWrap}>
 					<motion.div
-						className={styles.handle}
+						className={styles.item}
+						style={{
+							justifyContent: clicked ? 'flex-start' : 'flex-end',
+						}}
 						animate={{
 							backgroundColor: clicked
-								? 'var(--color-surface-300)'
-								: 'var(--color-accent-green)',
+								? 'var(--color-surface-200)'
+								: 'var(--color-text-100)',
 						}}
-						onClick={() => setClicked(!clicked)}
-						layout
-					></motion.div>
-				</motion.div>
-				<p>with layout animation</p>
+					>
+						<motion.div
+							className={styles.handle}
+							animate={{
+								backgroundColor: clicked
+									? 'var(--color-surface-300)'
+									: 'var(--color-accent-green)',
+							}}
+							onClick={() => setClicked(!clicked)}
+						></motion.div>
+					</motion.div>
+				</div>
+			)}
+
+			<div
+				style={{
+					position: 'absolute',
+					bottom: 20,
+					left: '50%',
+					transform: 'translateX(-50%)',
+				}}
+			>
+				<SingleCheckBox
+					value={layout}
+					setValue={setlayout}
+					id="layout"
+					content="layout"
+				/>
 			</div>
 		</div>
 	)

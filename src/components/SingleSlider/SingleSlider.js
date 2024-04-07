43d9e6457fe0f5807f227ca18b82044dc99adbb5
shuo <caihehuang@gmail.com>
2024-04-07 23:49:02 +0800

    Reapply "modify slider with active range"
    This reverts commit 9b58863e0b9a28526a58abe97a29931560b5bd6d.

--- src/components/SingleSlider/SingleSlider.js
+++ src/components/SingleSlider/SingleSlider.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Field from '../Field';
+import styles from './SingleSlider.module.scss'
+import { transform } from 'framer-motion';
 
 function SingleSlider({ label, id, min, max, step, value, setValue }) {
 
@@ -7,17 +9,27 @@ function SingleSlider({ label, id, min, max, step, value, setValue }) {
         const { id, value } = e.target
         setValue(value)
     }
+
+    let range = transform([min, max], [0, 1])
+    let rangeValue = range(value)
+
     return (
         <Field label={`${label}: ${value}`}>
-            <input
-                type="range"
-                id={id}
-                min={min}
-                max={max}
-                step={step}
-                value={value}
-                onChange={onChange}
-            />
+            <div className={styles.wrap}>
+                <span
+                    className={styles.activeRange}
+                    style={{ width: `calc(${rangeValue * 100}%)` }}></span>
+                <input
+                    type="range"
+                    id={id}
+                    min={min}
+                    max={max}
+                    step={step}
+                    value={value}
+                    onChange={onChange}
+                />
+            </div>
+
         </Field>
     );
 }
--- src/helper/svgAnimation-ExampleMapper.js
+++ src/helper/svgAnimation-ExampleMapper.js
@@ -3,13 +3,14 @@ import SVGPathKeyframe from "@/components/mdxExamples/svgAnimations/svgPath/SVGP
 import SVGLine2Circle from "@/components/mdxExamples/svgAnimations/svgPath/SVGLine2Circle"
 import SVGPathHamberger from "@/components/mdxExamples/svgAnimations/svgPath/SVGPathHamberger"
 import SVGPathProperties from "@/components/mdxExamples/svgAnimations/svgPathProperties/SVGPathProperties"
-
+import SVGPathProperties02 from "@/components/mdxExamples/svgAnimations/svgPathProperties/SVGPathProperties02"
 const svgAnimationExampleMapper = {
     SVGPathBasic,
     SVGPathKeyframe,
     SVGLine2Circle,
     SVGPathHamberger,
     SVGPathProperties,
+    SVGPathProperties02,
 }
 
 export default svgAnimationExampleMapper

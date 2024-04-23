cd2c76a7890aa6228eaa111f4751f714a7344ab6
shuo <caihehuang@gmail.com>
2024-04-23 14:21:53 +0800

    can't figure out why dragging jumps to a high value instantly

--- src/components/mdxExamples/motionValue/motionValueBasic/UseStateSlider/UseStateSlider.jsx
+++ src/components/mdxExamples/motionValue/motionValueBasic/UseStateSlider/UseStateSlider.jsx
@@ -7,17 +7,15 @@ import { useMeasure } from '@uidotdev/usehooks'
 
 export default function UseStateSlider() {
 	const [ref, { width: wrapWidth, height: wrapHeight }] = useMeasure()
-	const leftPanelRef = React.useRef(null)
 	const handleSize = 60
-	const handleRef = React.useRef(null)
 	const [initialHandleX, setInitialHandleX] = React.useState(0)
 	const [handleX, setHandleX] = React.useState(0)
-	const [leftPanelX, setLeftPanelX] = React.useState(0)
+
+	const handleRef = React.useRef()
 
 	React.useEffect(() => {
 		setInitialHandleX(handleRef.current.getBoundingClientRect().x)
 		setHandleX(handleRef.current.getBoundingClientRect().x)
-		setLeftPanelX(leftPanelRef.current.getBoundingClientRect().x)
 	}, [])
 
 	return (
@@ -26,19 +24,12 @@ export default function UseStateSlider() {
 				<motion.div
 					className={styles.leftPanel}
 					style={{
-						width:
-							handleX -
-							initialHandleX -
-							leftPanelX +
-							wrapWidth / 2 +
-							handleSize,
-
+						width: handleX - initialHandleX,
 						borderTopLeftRadius: (wrapHeight * 2) / 9,
 						borderBottomLeftRadius: (wrapHeight * 2) / 9,
 						borderTopRightRadius: (wrapHeight * 1) / 18,
 						borderBottomRightRadius: (wrapHeight * 1) / 18,
 					}}
-					ref={leftPanelRef}
 				>
 					<motion.div />
 				</motion.div>
@@ -65,10 +56,7 @@ export default function UseStateSlider() {
 				<motion.div
 					className={styles.rightPanel}
 					style={{
-						width:
-							wrapWidth / 2 -
-							(handleX - initialHandleX - leftPanelX) -
-							handleSize,
+						width: wrapWidth - (handleX - initialHandleX),
 						borderTopLeftRadius: (wrapHeight * 1) / 18,
 						borderBottomLeftRadius: (wrapHeight * 1) / 18,
 						borderTopRightRadius: (wrapHeight * 2) / 9,

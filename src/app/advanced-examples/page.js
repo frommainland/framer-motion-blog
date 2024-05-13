6178f4b72850bcf329da7a029536994f85d22faf
shuo <caihehuang@gmail.com>
2024-05-13 11:08:44 +0800

    update contents

--- src/app/advanced-examples/page.js
+++ src/app/advanced-examples/page.js
@@ -4,6 +4,7 @@ import React from 'react'
 import styles from './advancedexamples.module.scss'
 import IpadPointer from './components/ipadPointer/IpadPointer'
 import { CustomCursor } from './components/CustomCursor/CustomCursor'
+import { VelocityCursor } from './components/VelocityCursor/VelocityCursor'
 
 const Advancedexamples = () => {
     return (
@@ -12,16 +13,22 @@ const Advancedexamples = () => {
                 <div className={styles.layout}>
                     <h1>Advanced Examples</h1>
                     <section>
-                        <h2>iPad Pointer Effect</h2>
+                        <h3>iPad Pointer Effect</h3>
                         <p>When you hover on any icons, it feels the icon pull the cursor in when it is near. This magnetic feel animations is explained by Apple designers at WWDC20.</p>
                         <IpadPointer />
                     </section>
+
                     <section>
-                        <h2>Custom Cursor</h2>
+                        <h3>Custom Cursor</h3>
                         <p>Cursor controled perspective and enter effect</p>
                         <CustomCursor />
                     </section>
 
+                    <section>
+                        <h3>Velocity Cursor</h3>
+                        <p>You can access the velocity of a motion value and use it to skew shapes</p>
+                        <VelocityCursor />
+                    </section>
                 </div>
             </div>
         </div>
@@ -29,3 +36,4 @@ const Advancedexamples = () => {
 }
 
 export default Advancedexamples
+

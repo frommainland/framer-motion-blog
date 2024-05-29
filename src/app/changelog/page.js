8afe0217733181bdb54adc7fbadba96716ee2151
shuo <caihehuang@gmail.com>
2024-05-29 13:51:25 +0800

    Update page.js

--- src/app/changelog/page.js
+++ src/app/changelog/page.js
@@ -9,11 +9,20 @@ const ChangeLog = () => {
             <div className={styles.layoutWrap}>
                 <div className={styles.layout}>
                     <h1>Changelog</h1>
+                    {/* newer start here */}
+                    <section>
+                        <h2>Cursor examples</h2>
+                        <p className={styles.date}>May 24, 2024</p>
+                        <p>Add 6 examples of cursor effect in advanced examples, mixing framer motion multiple features.</p>
+                    </section>
+
                     <section>
                         <h2>Site launch</h2>
                         <p className={styles.date}>May 06, 2024</p>
                         <p>This site is alive, including the most basic examples of how framer motion works in web animations.</p>
                     </section>
+
+
 
                 </div>
             </div>
